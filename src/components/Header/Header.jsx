cece09db0ca35c6b3d0d1d43e1e4f43f51baa6af
ahkamulbd <ahkamulbd@gmail.com>
2023-06-12 20:26:47 +0600

    Module 50-2 done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,18 @@
-
+import React from "react";
+import './Header.css';
+import logo from '../../images/Logo.svg'
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is Header</h1>
-        </div>
+        <nav className="header">
+            <img src={logo} alt="" />
+            <div>
+                <a href="/order">order</a>
+                <a href="/review">review</a>
+                <a href="/inventory">inventory</a>
+                <a href="/login">login</a>
+            </div>
+        </nav>
     );
 };
 
